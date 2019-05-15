19908204b4e304a4103c899760a1a9aaf29886a5
marco <makoheuvelman@hotmail.com>
2019-05-15 15:15:04 +0200

    emergency push

--- assignment_3/code/a3_gan_template.py
+++ assignment_3/code/a3_gan_template.py
@@ -6,76 +6,168 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torchvision import datasets
-
+import numpy as np
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_dim):
         super(Generator, self).__init__()
 
+        self.latent_dim = latent_dim
+        self.layers = OrderedDict()
         # Construct generator. You are free to experiment with your model,
         # but the following is a good start:
-        #   Linear args.latent_dim -> 128
-        #   LeakyReLU(0.2)
-        #   Linear 128 -> 256
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 256 -> 512
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 512 -> 1024
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 1024 -> 768
-        #   Output non-linearity
+        self.layers["linear_0"] = nn.Linear(self.latent_dim, 128)
+        self.layers["leakyrelu_0"] = nn.LeakyReLU(negative_slope=0.02, inplace=True)
 
+        self.layers["linear_1"] = nn.Linear(128, 256)
+        self.layers["batchnorm_1"] = nn.BatchNorm1d(256)
+        self.layers["leakyrelu_1"] = nn.LeakyReLU(negative_slope=0.02, inplace=True)
+
+        self.layers["linear_2"] = nn.Linear(256, 512)
+        self.layers["batchnorm_2"] = nn.BatchNorm1d(512)
+        self.layers["leakyrelu_2"] = nn.LeakyReLU(negative_slope=0.02, inplace=True)
+
+        self.layers["linear_3"] = nn.Linear(512, 1024)
+        self.layers["batchnorm_3"] = nn.BatchNorm1d(1024)
+        self.layers["leakyrelu_3"] = nn.LeakyReLU(negative_slope=0.02, inplace=True)
+
+        self.layers["linear_4"] = nn.Linear(1024, 784)
+        self.layers["tanh_4"] = nn.Tanh()
+        # self.layers["tsig_4"] = nn.Sigmoid()
+        #   Output non-linearity ???
+
+        self.classifier = nn.Sequential(self.layers)
+        print("Created model : {}".format(self))
     def forward(self, z):
+
         # Generate images from z
-        pass
+        out = self.classifier(z)
+        return out
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
+        self.layers = OrderedDict()
+
         # Construct distriminator. You are free to experiment with your model,
         # but the following is a good start:
-        #   Linear 784 -> 512
-        #   LeakyReLU(0.2)
-        #   Linear 512 -> 256
-        #   LeakyReLU(0.2)
-        #   Linear 256 -> 1
-        #   Output non-linearity
+        self.layers["linear_0"] = nn.Linear(784, 512)
+        self.layers["leakyrelu_0"] = nn.LeakyReLU(negative_slope=0.02, inplace=True)
+        self.layers["dropout_0"] = nn.Dropout(0.3)
+
+        self.layers["linear_1"] = nn.Linear(512, 256)
+        self.layers["leakyrelu_1"] = nn.LeakyReLU(negative_slope=0.02, inplace=True)
+        self.layers["dropout_1"] = nn.Dropout(0.3)
+
+        self.layers["linear_2"] = nn.Linear(256, 1)
+        self.layers["sigmoid_2"] = nn.Sigmoid()
+        #   Output non-linearity ??
+
+        self.classifier = nn.Sequential(self.layers)
+        print("Created model : {}".format(self))
 
     def forward(self, img):
         # return discriminator score for img
-        pass
+        out = self.classifier(img)
+        return out
 
 
-def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
+def train(dataloader, discriminator, generator, optimizer_G, optimizer_D, args):
+
+    criterion = nn.BCELoss()
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else
+        device = torch.device("cpu")
+
+    z_to_follow = np.random.normal(loc=0.0, scale=1.0, size=(100, args.latent_dim))
+    z_to_follow = torch.from_numpy(z_to_follow).to(dtype=torch.float, device=device)
+
+    generator_losses = []
+    discriminator_losses = []
+    generator_losses_mean = []
+    discriminator_losses_mean = []
+
+    discriminator.to(device)
+    generator.to(device)
+
     for epoch in range(args.n_epochs):
         for i, (imgs, _) in enumerate(dataloader):
 
-            imgs.cuda()
+            # ones = torch.ones((imgs.shape[0],1), dtype=torch.float, device=device)
+            zeros_ = torch.zeros((imgs.shape[0],1), dtype=torch.float, device=device)
 
-            # Train Generator
-            # ---------------
+            ones = torch.FloatTensor(imgs.shape[0],1).uniform_(0.7, 1.2).to(dtype=torch.float, device=device)
+            zeros = torch.FloatTensor(imgs.shape[0],1).uniform_(0, 0.3).to(dtype=torch.float, device=device)
+            answers = torch.cat((ones, zeros), dim=0)
+
+            z = np.random.normal(loc=0.0, scale=1.0, size=(imgs.shape[0], args.latent_dim))
+            z = torch.from_numpy(z).to(dtype=torch.float, device=device)
+            generated_imgs = generator.forward(z)
+            predictions_generated = discriminator.forward(generated_imgs)
+
+            imgs = imgs.to(device)
+            imgs = imgs.view(imgs.shape[0], -1)
+            predictions_real = discriminator.forward(imgs)
+            predictions = torch.cat((predictions_generated, predictions_real), dim=0)
 
             # Train Discriminator
             # -------------------
             optimizer_D.zero_grad()
+            discriminator_loss = criterion(predictions, answers)
+            discriminator_loss.backward(retain_graph=True)
+            optimizer_D.step()
+
+            # Train Generator
+            # ---------------
+            optimizer_G.zero_grad()
+            generator_loss = criterion(predictions_generated, zeros_)
+            generator_loss.backward()
+            optimizer_G.step()
+
+            generator_losses.append(generator_loss.item())
+            discriminator_losses.append(discriminator_loss.item())
 
             # Save Images
             # -----------
             batches_done = epoch * len(dataloader) + i
             if batches_done % args.save_interval == 0:
-                # You can use the function save_image(Tensor (shape Bx1x28x28),
-                # filename, number of rows, normalize) to save the generated
-                # images, e.g.:
-                # save_image(gen_imgs[:25],
-                #            'images/{}.png'.format(batches_done),
-                #            nrow=5, normalize=True)
-                pass
 
+                generator_losses_mean.append(np.mean(generator_losses))
+                discriminator_losses_mean.append(np.mean(discriminator_losses))
+                generator_losses = []
+                discriminator_losses = []
+
+                torch.save(generator.state_dict(), args.load_generator)
+
+                with torch.no_grad():
+
+                    if True:
+                        path = 'images/Losses.png'
+                        plt.plot(generator_losses_mean, label='generator loss')
+                        plt.plot(discriminator_losses_mean, label='discriminator loss')
+                        plt.title("Training losses GAN")
+                        plt.xlabel("Epochs")
+                        plt.ylabel("Loss")
+                        plt.legend()
+                        plt.savefig(path)
+                        plt.close('all')
+
+                    if True:
+                        generated_imgs = generator.forward(z_to_follow)
+                        to_show = generated_imgs.view(-1,1,28,28)
+                        save_image(to_show,
+                                   'images/{}.png'.format(batches_done),
+                                   nrow=10, normalize=True)
+
+
+                    print("epoch: {}".format(epoch))
 
 def main():
     # Create output image directory
@@ -86,23 +178,45 @@ def main():
         datasets.MNIST('./data/mnist', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
-                           transforms.Normalize((0.5, 0.5, 0.5),
-                                                (0.5, 0.5, 0.5))])),
+                           transforms.Normalize((0.5,),
+                                                (0.5,))])),
         batch_size=args.batch_size, shuffle=True)
 
     # Initialize models and optimizers
-    generator = Generator()
+    generator = Generator(args.latent_dim)
     discriminator = Discriminator()
     optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr)
     optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr)
-
+    # optimizer_D = torch.optim.SGD(discriminator.parameters(), lr=args.lr)
     # Start training
-    train(dataloader, discriminator, generator, optimizer_G, optimizer_D)
+
+
+
+    if Path(args.load_generator).exists():
+        generator.load_state_dict(torch.load(args.load_generator))
+
+    train(dataloader, discriminator, generator, optimizer_G, optimizer_D, args)
 
     # You can save your generator here to re-use it to generate images for your
     # report, e.g.:
     # torch.save(generator.state_dict(), "mnist_generator.pt")
 
+def plot_figures(figures, nrows = 1, ncols=1):
+    """Plot a dictionary of figures.
+
+    Parameters
+    ----------
+    figures : <title, figure> dictionary
+    ncols : number of columns of subplots wanted in the display
+    nrows : number of rows of subplots wanted in the figure
+    """
+
+    fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows)
+    for ind,title in enumerate(figures):
+        axeslist.ravel()[ind].imshow(figures[title], cmap=plt.gray())
+        # axeslist.ravel()[ind].set_title(title)
+        axeslist.ravel()[ind].set_axis_off()
+    plt.tight_layout() # optional
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -116,6 +230,8 @@ if __name__ == "__main__":
                         help='dimensionality of the latent space')
     parser.add_argument('--save_interval', type=int, default=500,
                         help='save every SAVE_INTERVAL iterations')
+    parser.add_argument('--load_generator', type=str, default="mnist_generator.pt",
+                        help='Load generator give path')
     args = parser.parse_args()
 
     main()
--- assignment_3/code/a3_vae_template.py
+++ assignment_3/code/a3_vae_template.py
@@ -1,17 +1,24 @@
 import argparse
-
+import numpy as np
 import torch
+import torch.nn.functional as F
 import torch.nn as nn
 import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
-
+from collections import OrderedDict
 from datasets.bmnist import bmnist
-
+from torchvision.utils import save_image
 
 class Encoder(nn.Module):
 
     def __init__(self, hidden_dim=500, z_dim=20):
         super().__init__()
+
+        self.hidden = nn.Linear(784, hidden_dim)
+        self.mu = nn.Linear(hidden_dim, z_dim)
+        self.std = nn.Linear(hidden_dim, z_dim)
+
+        print("Created model : {}".format(self))
 
     def forward(self, input):
         """
@@ -20,9 +27,10 @@ class Encoder(nn.Module):
         Returns mean and std with shape [batch_size, z_dim]. Make sure
         that any constraints are enforced.
         """
-        mean, std = None, None
-        raise NotImplementedError()
 
+        hidden = torch.tanh(self.hidden(input.view(input.shape[0],-1)))
+        mean = self.mu(hidden)
+        std = self.std(hidden)
         return mean, std
 
 
@@ -31,34 +39,89 @@ class Decoder(nn.Module):
     def __init__(self, hidden_dim=500, z_dim=20):
         super().__init__()
 
+        self.layers = OrderedDict()
+        self.layers["linear_0"] = nn.Linear(z_dim, hidden_dim)
+        self.layers["tanh_0"] = nn.Tanh()
+        self.layers["linear_1"] = nn.Linear(hidden_dim, 784)
+        self.layers["sigmoid_1"] = nn.Sigmoid()
+
+        self.decode = nn.Sequential(self.layers)
+
+        print("Created model : {}".format(self))
+
     def forward(self, input):
         """
         Perform forward pass of encoder.
 
         Returns mean with shape [batch_size, 784].
         """
-        mean = None
-        raise NotImplementedError()
+        mean = self.decode(input)
 
         return mean
 
 
 class VAE(nn.Module):
 
-    def __init__(self, hidden_dim=500, z_dim=20):
+    def __init__(self, hidden_dim=500, z_dim=20, device='cuda'):
         super().__init__()
 
+        self.device = device
         self.z_dim = z_dim
-        self.encoder = Encoder(hidden_dim, z_dim)
-        self.decoder = Decoder(hidden_dim, z_dim)
+        self.encoder = Encoder(hidden_dim, z_dim).to(device)
+        self.decoder = Decoder(hidden_dim, z_dim).to(device)
 
     def forward(self, input):
         """
         Given input, perform an encoding and decoding step and return the
         negative average elbo for the given batch.
         """
-        average_negative_elbo = None
-        raise NotImplementedError()
+
+        mean, variance_log = self.encoder.forward(input)
+
+        self.mean = mean
+        """
+            STD is actually log variance?
+        """
+        # print(mean)
+        std = torch.exp(variance_log*0.5)
+        zeros = torch.zeros(mean.shape).to(self.device)
+        noise = torch.normal(zeros, std).to(self.device)
+        # noise = torch.randn_like(std)
+        z = mean + noise.mul(std)
+
+        """
+            z should be the mean (MLE) plus some noise (variation)
+        """
+        # print(z)
+        output = self.decoder.forward(z)
+        # print(output.shape)
+        # print(asdasd)
+        if True:
+            to_show = output[:100].view(-1,1,28,28)
+            save_image(to_show,
+                       'images/VAE_{}.png'.format(0),
+                       nrow=10, normalize=True)
+        # plt.matshow(input[0].view(28,28).cpu().detach().numpy())
+        # plt.show()
+        # print(asdasd)
+        """
+            Kullback–Leibler divergence
+        """
+        # ????
+        # The loss for the modelling the latent space (generative part)
+        kl = -0.5 * torch.sum(1 + variance_log - mean**2 - variance_log.exp())
+
+        # the loss for reconstruction (the autoencoder part)
+
+        # using magic numbers is bad bad but whatever
+        # recon_loss = nn.BCELoss(output, input.view(input.shape[0], -1), reduction='sum')
+        # Should reconstruction be sum or mean? I can understand that summing would make things more stringent for reconstruction
+        recon_loss = F.binary_cross_entropy(output, input.view(input.shape[0], -1), reduction='sum')
+        # print("recon: {}, kl: {}".format(recon_loss.item(), kl.item()))
+        # the KL loss is always about 20% of the recon_loss.... I wonder if it should be higher?
+        # print(kl.item())
+        average_negative_elbo = kl*5 + recon_loss
+
         return average_negative_elbo
 
     def sample(self, n_samples):
@@ -68,35 +131,59 @@ class VAE(nn.Module):
         used to plot the data manifold).
         """
         sampled_ims, im_means = None, None
-        raise NotImplementedError()
+
+        with torch.no_grad():
+
+            # We repeat the mean vector for as many samples as we want
+            # The shape will be (n_samples, length_of_latent)
+            to_bernoullio = self.mean.repeat(n_samples, 1)
+            # We then sample using those means, note means must be between
+            # 0 and 1 for this to work. Returns binary values.
+            z = torch.bernoulli(to_bernoullio)
+
+            # push through these values through decoder
+            sampled_ims = self.decoder.forward(z)
+            # unflatten the vector into an image (black and white)
+            sampled_ims = sampled_ims.view(-1, 1, 28, 28)
 
         return sampled_ims, im_means
 
 
-def epoch_iter(model, data, optimizer):
+def epoch_iter(model, data, optimizer, device):
     """
     Perform a single epoch for either the training or validation.
     use model.training to determine if in 'training mode' or not.
 
     Returns the average elbo for the complete epoch.
     """
-    average_epoch_elbo = None
-    raise NotImplementedError()
+    # make list of all batches, then average at end
+    average_epoch_elbo = []
 
-    return average_epoch_elbo
+    # dataloader data already gives you batches, usually size 128, but can be smaller
+    for batch in data:
+        batch = batch.to(device)
+        elbo_batch = model.forward(batch)
+        # print(asdasds)
+        average_epoch_elbo.append(elbo_batch.item())
+        if model.training:
+            optimizer.zero_grad()
+            elbo_batch.backward()
+            optimizer.step()
+
+    return np.mean(average_epoch_elbo)
 
 
-def run_epoch(model, data, optimizer):
+def run_epoch(model, data, optimizer, device):
     """
     Run a train and validation epoch and return average elbo for each.
     """
     traindata, valdata = data
 
     model.train()
-    train_elbo = epoch_iter(model, traindata, optimizer)
+    train_elbo = epoch_iter(model, traindata, optimizer, device)
 
     model.eval()
-    val_elbo = epoch_iter(model, valdata, optimizer)
+    val_elbo = epoch_iter(model, valdata, optimizer, device)
 
     return train_elbo, val_elbo
 
@@ -113,13 +200,19 @@ def save_elbo_plot(train_curve, val_curve, filename):
 
 
 def main():
+
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+
     data = bmnist()[:2]  # ignore test split
-    model = VAE(z_dim=ARGS.zdim)
+    model = VAE(z_dim=ARGS.zdim, device=device).to(device)
     optimizer = torch.optim.Adam(model.parameters())
 
     train_curve, val_curve = [], []
     for epoch in range(ARGS.epochs):
-        elbos = run_epoch(model, data, optimizer)
+        elbos = run_epoch(model, data, optimizer, device)
         train_elbo, val_elbo = elbos
         train_curve.append(train_elbo)
         val_curve.append(val_elbo)
